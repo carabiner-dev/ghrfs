9425b1bc127aaa99695771a9e4a95786a92c7197
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-03-17 23:30:31 -0600

    Update demo
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- demo/README.md
+++ demo/README.md
@@ -3,7 +3,7 @@
 This directory contains a [simple program](main.go) that creates a filesystem 
 out of a release[^1].
 
-The demo program creates the filesystem, opens a file, prints its attributes and
-reads its contents.
+The demo program creates the filesystem, runs a Walk through it, opens a file,
+prints its attributes and reads its contents.
 
 [^1]: The test release is this project's [v0.0.0 release](https://github.com/carabiner-dev/ghrfs/releases/tag/v0.0.0) which is designed to run its tests.
--- demo/main.go
+++ demo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 
 	"github.com/carabiner-dev/ghrfs"
@@ -18,6 +19,33 @@ func main() {
 		os.Exit(1)
 	}
 
+	fmt.Print("Walking filesystem:\n")
+	// Walk the filesystem
+	if err := fs.WalkDir(rfs, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return fmt.Errorf("scanning at %s: %w", path, err)
+		}
+
+		if d.IsDir() {
+			fmt.Printf("%s/ (%s)\n", path, d.Name())
+			return nil
+		}
+
+		// Read the file data from the filesystem
+		info, err := fs.Stat(rfs, path)
+		if err != nil {
+			return fmt.Errorf("reading file from fs: %w", err)
+		}
+
+		fmt.Printf("  %s (%d bytes)\n", path, info.Size())
+
+		return nil
+	}); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Print("\nOpening Test File:\n")
 	// Open a file from the release:
 	file, err := rfs.Open("about-this-release.txt")
 	if err != nil {
@@ -33,8 +61,8 @@ func main() {
 	}
 
 	// Do something with the file:
-	fmt.Printf("%s atrributes: %+v\n", info.Name(), info)
-	fmt.Println("Test File Contents:")
+	fmt.Printf("  %s atrributes:\n  %+v\n", info.Name(), info)
+	fmt.Println("\nTest File Contents:\n")
 	if _, err := io.Copy(os.Stdout, file); err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
